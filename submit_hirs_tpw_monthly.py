a2a27b6137b82e0c3c516928499ca6a6c014377f
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2017-01-19 21:37:43 +0000

    Cleanup of the HIRS cluster submission scripts.

--- submit_hirs_tpw_monthly.py
+++ submit_hirs_tpw_monthly.py
@@ -8,9 +8,6 @@ from calendar import monthrange
 import logging
 import traceback
 
-from subprocess import CalledProcessError, call
-from subprocess import Popen, STDOUT, PIPE
-
 from flo.time import TimeInterval
 from flo.ui import safe_submit_order
 from flo.product import StoredProductCatalog
@@ -35,11 +32,11 @@ comp = HIRS_TPW_MONTHLY()
 SPC = StoredProductCatalog()
 
 # Latest Computation versions.
-hirs_version  = 'v20151014'
+hirs_version = 'v20151014'
 collo_version = 'v20151014'
-csrb_version  = 'v20150915'
-ctp_version   = 'v20150915'
-tpw_version   = 'v20160222'
+csrb_version = 'v20150915'
+ctp_version = 'v20150915'
+tpw_version = 'v20160222'
 
 platform_choices = ['noaa-06', 'noaa-07', 'noaa-08', 'noaa-09', 'noaa-10', 'noaa-11',
                     'noaa-12', 'noaa-14', 'noaa-15', 'noaa-16', 'noaa-17', 'noaa-18',
@@ -64,6 +61,7 @@ wedge = timedelta(seconds=1.)
     #TimeInterval(datetime(2016, 12, 1),datetime(2017, 1, 1) - wedge),
 #]
 
+# Examine how many of the defined contexts are populated
 intervals = []
 year,month = 2016,5
 months = range(1, 12 + 1)
@@ -76,7 +74,7 @@ for month in months:
                                   tpw_version, interval)
     num_contexts_exist = 0
     for context in contexts:
-        num_contexts_exist += SPC.exists(comp.dataset('zonal_means').product(context))
+        num_contexts_exist += SPC.exists(comp.dataset('out').product(context))
     LOG.info("Interval {} has {}/{} contexts existing".format(interval, num_contexts_exist, len(contexts)))
     missing_contexts = len(contexts) - num_contexts_exist
     if missing_contexts == 1:
@@ -89,8 +87,8 @@ for interval in intervals:
                                   tpw_version, interval)
     LOG.info("\tThere are {} contexts in this interval".format(len(contexts)))
     contexts.sort()
-    #for context in contexts:
-        #LOG.debug(context)
+    for context in contexts:
+        LOG.debug(context)
     LOG.info("\tFirst context: {}".format(contexts[0]))
     LOG.info("\tLast context:  {}".format(contexts[-1]))
     LOG.info("\t{}".format(safe_submit_order(comp,
